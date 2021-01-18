efb9b8243487b62ae6b04fc1e454b3f0f27be5e9
bartek <mzyku12@gmail.com>
2021-01-18 22:20:54 +0100

    Database configuration implemented

--- src/main/java/com/wetorek/teamproject/TeamProjectApplication.java
+++ src/main/java/com/wetorek/teamproject/TeamProjectApplication.java
@@ -2,12 +2,17 @@ package com.wetorek.teamproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
 
 @SpringBootApplication
-public class TeamProjectApplication {
-
+public class TeamProjectApplication implements ApplicationListener<ContextRefreshedEvent> {
     public static void main(String[] args) {
         SpringApplication.run(TeamProjectApplication.class, args);
     }
 
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+
+    }
 }
